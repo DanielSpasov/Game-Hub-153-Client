2fdb2e48d017e5dde47474db12d0493b9823d3eb
ShadyGotRabies <danielspasov4@gmail.com>
2021-03-13 16:54:01 +0200

    Body component added.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import './App.css'
 
 import Navbar from './components/Navbar/Navbar'
+import Body from './components/Body/Body'
 
 function App() {
     return (
         <div className="app">
 
             <Navbar />
+            <Body />
             
         </div>
     )
---
+++ client/src/components/Body/Body.js
@@ -0,0 +1,17 @@
+import './Body.css'
+
+function Body() {
+    return (
+        <main className="main-container">
+
+            <div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam libero inventore accusantium obcaecati deleniti? Error dolore in dignissimos minus earum, eos neque consequatur velit unde, illo facilis! Incidunt, minima nesciunt.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam libero inventore accusantium obcaecati deleniti? Error dolore in dignissimos minus earum, eos neque consequatur velit unde, illo facilis! Incidunt, minima nesciunt.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam libero inventore accusantium obcaecati deleniti? Error dolore in dignissimos minus earum, eos neque consequatur velit unde, illo facilis! Incidunt, minima nesciunt.</p>
+            </div>
+
+        </main>
+    )
+}
+
+export default Body
