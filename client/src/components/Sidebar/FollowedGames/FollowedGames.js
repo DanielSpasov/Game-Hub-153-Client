6f0e722b3c4baaddf9dfe73e24b3d31b80bb5896
ShadyGotRabies <danielspasov4@gmail.com>
2021-03-15 12:09:31 +0200

    Sidebar items added & sidebar is now responsive.

---
+++ client/src/components/Sidebar/FollowedGames/FollowedGames.js
@@ -0,0 +1,16 @@
+import './FollowedGames.css'
+
+function FollowedGames() {
+    return (
+        <div className="followed-games-container">
+            <ul className="followed-games-list">
+                <h3>Followed games</h3>
+                <a href="#/"><li>The Witcher 3</li></a>
+                <a href="#/"><li>Overwatch</li></a>
+                <a href="#/"><li>VALORANT</li></a>
+            </ul>
+        </div>
+    )
+}
+
+export default FollowedGames
--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -1,10 +1,12 @@
 import './Sidebar.css'
 
+import FollowedGames from './FollowedGames/FollowedGames'
+
 function Sidebar() {
     return (
         <aside className="sidebar">
 
-            <p>sidebar go brr</p>
+            <FollowedGames />
 
         </aside>
     )
