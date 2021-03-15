ddbc8b09985d3b4c3f24699dee87339bdf995dc2
ShadyGotRabies <danielspasov4@gmail.com>
2021-03-15 09:11:10 +0200

    Added more navigation items.

--- client/src/components/Navbar/DropdownMenu/Options/Options.js
+++ client/src/components/Navbar/DropdownMenu/Options/Options.js
@@ -6,18 +6,27 @@ function Options() {
     return (
         <div className="dropdown-content">
 
-            <h1>TODO:</h1>
+            <h1>TODO: User</h1>
             <ul>
                 <Item href="#/">Profile Settings</Item>
-                <Item href="#/">Light Mode</Item>
-                <Item href="#/">Logout</Item>
-                <Item href="#/">Login</Item>
                 <Item href="#/">Register</Item>
-                <Item href="#/">Categories</Item>
-                <Item href="#/">Add Game Page</Item>
-                <Item href="#/">Add Category Page</Item>
+                <Item href="#/">Login</Item>
+                <Item href="#/">Logout</Item>
             </ul>
-            
+
+            <h1>TODO: Functionality</h1>
+            <ul>
+                <Item href="#/">Follow Games</Item>
+                <Item href="#/">Genres</Item>
+                <Item href="#/">Games</Item>
+                <Item href="#/">Developers</Item>
+            </ul>
+
+            <h1>TODO: Misc</h1>
+            <ul>
+                <Item href="#/">Light Mode</Item>
+            </ul>
+
         </div>
     )
 }
--- client/src/components/Navbar/Navigation/Navigation.js
+++ client/src/components/Navbar/Navigation/Navigation.js
@@ -5,8 +5,10 @@ import Item from './Item/Item'
 function Navigation() {
     return (
         <div className="navigation">
-            <Item href="#/categories" text="Categories" id="categories" />
+            <Item href="#/followGames" text="Follow Games" id="follow-games" />
+            <Item href="#/genres" text="Genres" id="genres" />
             <Item href="#/games" text="Games" id="games" />
+            <Item href="#/developers" text="Developers" id="developers" />
         </div>
     )
 }
