54e3c371bbb13c753fbd93a06314909cf345f58f
ShadyGotRabies <danielspasov4@gmail.com>
2021-03-16 08:55:05 +0200

    Top 5 Games and Genres are now responsive.

--- client/src/components/Body/HomePage/TopFiveGames/TopFiveGames.js
+++ client/src/components/Body/HomePage/TopFiveGames/TopFiveGames.js
@@ -8,10 +8,14 @@ function TopFiveGames({
 }) {
     return (
         <div className="top-five-games">
+
             <h2>Top 5 Most Upvoted Games</h2>
-            {games.map(x =>
-                <Game key={x._id} title={x.title} imageUrl={x.imageUrl} />
-            )}
+            
+            <div className="game-container">
+                {games.map(x =>
+                    <Game key={x._id} title={x.title} imageUrl={x.imageUrl} />
+                )}
+            </div>
         </div>
     )
 }
--- client/src/components/Body/HomePage/TopFiveGenres/TopFiveGenres.js
+++ client/src/components/Body/HomePage/TopFiveGenres/TopFiveGenres.js
@@ -2,15 +2,20 @@ import './TopFiveGenres.css'
 
 import Genre from '../../../Reuseables/Genre/Genre'
 
+
 function TopFiveGenres({
     genres
 }) {
     return (
         <div className="top-five-genres">
+
             <h2>Top 5 Most Played Genres</h2>
-            {genres.map(x =>
-                <Genre key={x._id} name={x.name} />
-            )}
+            
+            <div className="genres-container">
+                {genres.map(x =>
+                    <Genre key={x._id} name={x.name} />
+                )}
+            </div>
         </div>
     )
 }
