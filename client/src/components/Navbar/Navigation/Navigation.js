ff197c6430a5693446c7f920d6fb63fcf2ff030b
ShadyGotRabies <danielspasov4@gmail.com>
2021-03-17 15:25:28 +0200

    Removed follow games from navbar.

--- client/src/components/Navbar/Navigation/Navigation.js
+++ client/src/components/Navbar/Navigation/Navigation.js
@@ -5,9 +5,8 @@ import Item from './Item/Item'
 function Navigation() {
     return (
         <div className="navigation">
-            <Item href="/followGames" text="Follow Games" id="follow-games" />
-            <Item href="/genres" text="Genres" id="genres" />
             <Item href="/games" text="Games" id="games" />
+            <Item href="/genres" text="Genres" id="genres" />
             <Item href="/devs" text="Developers" id="devs" />
         </div>
     )
