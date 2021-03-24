cfee86e947a290c02361fc107949755cb0c64c6f
ShadyGotRabies <danielspasov4@gmail.com>
2021-03-24 08:39:14 +0200

    Error messages css moved from games to app.

--- client/src/components/Body/Games/AddGames/AddGames.js
+++ client/src/components/Body/Games/AddGames/AddGames.js
@@ -4,7 +4,6 @@ import { ToastContainer, toast } from 'react-toastify'
 
 import gamesService from '../../../../services/gamesService'
 
-import 'react-toastify/dist/ReactToastify.css';
 import './AddGames.css'
 
 class AddGames extends Component {
