f635e55619ecb3c746593d4ea6cb670edfc5c112
ShadyGotRabies <danielspasov4@gmail.com>
2021-03-30 11:25:51 +0300

    Deleted follow pages.

--- client/src/components/Body/Body.js
+++ client/src/components/Body/Body.js
@@ -6,15 +6,12 @@ import './Body.css'
 import HomePage from './HomePage/HomePage.js'
 
 import Games from './Games/Games'
-import FollowGames from './Games/FollowGames/FollowGames'
 import AddGames from './Games/AddGames/AddGames'
 
 import Genres from './Genres/Genres'
-import FollowGenres from './Genres/FollowGenres/FollowGenres'
 import AddGenres from './Genres/AddGenres/AddGenres'
 
 import Devs from './Devs/Devs'
-import FollowDevs from './Devs/FollowDevs/FollowDevs'
 import AddDevs from './Devs/AddDevs/AddDevs'
 
 import Login from './Login/Login'
@@ -31,15 +28,12 @@ function Body({ setAppState }) {
                 <Route path="/" component={HomePage} exact />
 
                 <Route path="/games" component={Games} exact />
-                <Route path="/games/follow" component={FollowGames} exact />
                 <Route path="/games/add" component={AddGames} exact />
 
                 <Route path="/genres" component={Genres} exact />
-                <Route path="/genres/follow" component={FollowGenres} exact />
                 <Route path="/genres/add" component={AddGenres} exact />
 
                 <Route path="/devs" component={Devs} exact />
-                <Route path="/devs/follow" component={FollowDevs} exact />
                 <Route path="/devs/add" component={AddDevs} exact />
 
 
