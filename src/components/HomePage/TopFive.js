8f69387a7880a719fd8def5a449230daa81ce686
ShadyGotRabies <danielspasov4@gmail.com>
2021-04-15 09:33:41 +0300

    Changed typo in the home page.

--- src/components/HomePage/TopFive.js
+++ src/components/HomePage/TopFive.js
@@ -11,15 +11,15 @@ const TopFive = ({
     let itemsContainer
 
     if (category === 'games') {
-        title = <h2>Our top 5 most followed games</h2>
+        title = <h2>Our top 5 most upvoted games</h2>
         itemsContainer = <div>{items?.map(x => <Game id={x.id} key={x.id} title={x.title} imageUrl={x.imageUrl} />)}</div>
     }
     if (category === 'genres') {
-        title = <h2>Our top 5 most followed genres</h2>
+        title = <h2>Our top 5 most upvoted genres</h2>
         itemsContainer = <div>{items?.map(x => <Genre id={x.id} key={x.id} name={x.name} imageUrl={x.imageUrl} />)}</div>
     }
     if (category === 'devs') {
-        title = <h2>Our top 5 most followed game developers</h2>
+        title = <h2>Our top 5 most upvoted game developers</h2>
         itemsContainer = <div>{items?.map(x => <Dev id={x.id} key={x.id} orgName={x.orgName} imageUrl={x.imageUrl} />)}</div>
     }
 
