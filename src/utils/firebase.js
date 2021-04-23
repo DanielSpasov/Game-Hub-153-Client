40c595c5cb71607e2a6014e3fd6bd28cab867c65
ShadyGotRabies <danielspasov4@gmail.com>
2021-04-23 08:11:30 +0300

    Removed Logged In / Logged Out message from firebase.

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -15,14 +15,6 @@ if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig)
 }
 
-firebase.auth().onAuthStateChanged((user) => {
-    if(user) {
-        console.log('Logged In')
-    } else {
-        console.log('Logged Out')
-    }
-})
-
 export default firebase
 export const auth = firebase.auth()
 export const db = firebase.firestore()
