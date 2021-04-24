9f1066ae5845b4cc127a9bb79988cf1ff78891e6
ShadyGotRabies <danielspasov4@gmail.com>
2021-04-24 10:06:07 +0300

    Made all protected routes exact.

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -3,18 +3,9 @@ import { useContext } from 'react'
 
 import UserContext from '../../contexts/UserContext'
 
-const ProtectedRoute = ({ exact, path, component: Component }) => {
-
+const ProtectedRoute = ({ path, component: Component }) => {
     const { isAuth } = useContext(UserContext)
-    let protectedRoute = <p>g</p>
-    
-    if (exact) {
-        protectedRoute = <Route exact path={path} render={() => isAuth ? <Component /> : <Redirect to="/user/login" />} />
-    } else {
-        protectedRoute = <Route path={path} render={() => isAuth ? <Component /> : <Redirect to="/user/login" />} />
-    }
-    
-    return protectedRoute
+    return <Route exact path={path} render={() => isAuth ? <Component /> : <Redirect to="/user/login" />} />
 }
 
 export default ProtectedRoute
